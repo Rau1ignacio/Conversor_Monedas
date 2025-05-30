7324a66bb54214b9b05182a4c3a8765add0b4bdb
Rau1ignacio <rau.bustamante@duocuc.cl>
2025-05-30 17:57:21 -0400

    Primer commit: Conversor de monedas

---
+++ main.py
@@ -0,0 +1,28 @@
+
+## API : https://api.exchangerate.host/convert?from=USD&to=EUR&amount=100
+
+import requests # Importa la librería requests para hacer peticiones HTTP
+
+def convertir_moneda(cantidad, de_moneda, a_moneda):
+    url = f"https://api.exchangerate.host/convert?from={de_moneda}&to={a_moneda}&amount={cantidad}"
+
+    response = requests.get(url)  # Realiza una petición GET a la API
+    data = response.json()  # Convierte la respuesta a formato JSON
+
+    if data['success']:
+        resultado = data['result']
+        print(f"{cantidad} {de_moneda} = {resultado:.2f} {a_moneda}")
+    else:
+        print("Error al obtener la conversión de moneda.")
+
+if __name__ == "__main__":
+    print("\n ============== Conversor de Monedas ==============")
+
+    print("\nBienvenido al conversor de monedas.")
+          
+    cantidad = float(input("Ingrese la cantidad a convertir: "))
+    de_moneda = input("Ingrese la moneda de origen (ej. USD): ").upper()
+    a_moneda = input("Ingrese la moneda de destino (ej. EUR): ").upper()
+
+    convertir_moneda(cantidad, de_moneda, a_moneda)
+
